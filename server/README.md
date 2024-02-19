619cfa6d5b38164deedbf6cdeb1d68d6128e6ddb
Entropy <70720874+NavinKumarMNK@users.noreply.github.com>
2024-02-19 07:48:01 +0530

    docker compose for postgres, django

--- server/README.md
+++ server/README.md
@@ -1,1 +1,11 @@
 # AI-Learning Platform
+
+## Installation
+```bash
+poetry install
+```
+
+## Start the Server
+```bash
+poetry run python manage.py runserver
+```
--- server/main/settings.py
+++ server/main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
