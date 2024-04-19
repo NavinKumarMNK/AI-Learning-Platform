b8171025d4bafed87b4fa19ecc8a5f4e29483f17
Entropy <70720874+NavinKumarMNK@users.noreply.github.com>
2024-04-20 03:16:47 +0530

    server linked with ml-service

--- server/api/v1/chat/views.py
+++ server/api/v1/chat/views.py
@@ -1,41 +1,45 @@
 import asyncio
+import logging
 
 from django.http import StreamingHttpResponse
+from asgiref.sync import async_to_sync
+from django.conf import settings
+
 from rest_framework import generics, mixins, status
 from rest_framework.response import Response
 
 from .models import Chat
 from .serializers import ChatSerializer
+from typing import Dict
 
-from asgiref.sync import sync_to_async, async_to_sync
-from django.conf import settings
+from meglib.ml.store import VectorDB
+from meglib.ml.api import Llm, Embedding
 
-logger = settings.LOGGER
-
-
-# outdated
-async def qdrant(message_emb):
-    return "Yo, Apples are red."
-
-
-async def llm(prompt):
-    return "Red color bro"
+logger = logging.getLogger("django")
+llm_api: Llm = settings.LLM_API
+embed_api: Embedding = settings.EMBED_API
+qdrant_db: VectorDB = settings.QDRANT_DB
+qdrant_config: Dict = settings.QDRANT_CONFIG
+llm_config: Dict = settings.LLM_CONFIG
 
 
 class ChatCreateAPIView(mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
 
-    def post(self, request, *args, **kwargs):
+    @async_to_sync
+    async def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        await self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
+        data = serializer.data
+        return_data = {"chat_id": data["chat_id"], "messages": data["messages"]}
         return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+            data=return_data, status=status.HTTP_201_CREATED, headers=headers
         )
 
-    def perform_create(self, serializer):
+    async def perform_create(self, serializer):
         serializer.save()
 
 
@@ -44,43 +48,110 @@ class ChatRetrieveAPIView(mixins.RetrieveModelMixin, generics.GenericAPIView):
     serializer_class = ChatSerializer
     lookup_field = "chat_id"
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+    @async_to_sync
+    async def get(self, request, *args, **kwargs):
+        response = self.retrieve(request, *args, **kwargs)
+        logger.warning(response)
+        return_data = {
+            "messages": response.data["messages"],
+            "title": response.data["title"],
+        }
+        return Response(
+            return_data,
+            status=status.HTTP_200_OK,
+        )
 
 
-class ChatUpdateAPIView(mixins.UpdateModelMixin, generics.GenericAPIView):
+class ChatCompletionAPIView(mixins.UpdateModelMixin, generics.GenericAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
     lookup_field = "chat_id"
 
     @async_to_sync
-    async def patch(self, request, *args, **kwargs):
+    async def post(self, request, *args, **kwargs):
         chat = self.get_object()
-        message = request.data.get("messages")
+        payload: Dict = request.data
+        messages = request.data["messages"]
+        logger.info(payload)
 
-        if message:
-            # Retrieving related content from qdrant
-            message_emb = "emb"  # await get_embeddings(message) - api changed
-            message_related_content = await qdrant(message_emb)
+        # retrieve embedding for message
+        embedding_msg = ""
+        for message in messages[:-3] or messages:
+            embedding_msg += "\n" + message["content"].strip()
 
-            # Generating prompt and getting response from llm
-            prompt = (
-                "Related Content: "
-                + message_related_content
-                + "\n"
-                + "Question: "
-                + message[0].get("content")
+        embedding: Dict = await embed_api.query(
+            payload={
+                "data": embedding_msg,
+                "type": "QUERY_EMBED",
+            }
+        )
+        embedding = embedding["embedding"]
+
+        # vector search
+        retrieved_content: Dict = qdrant_db.proto_to_dict(
+            await qdrant_db.search(
+                collection_name=qdrant_config["main"]["collection_name"],
+                vector=embedding,
+                limit=qdrant_config["infer"]["limit"],
+                filters={"course": payload["course"]},
             )
-            llm_response = await llm(prompt)
+        )
+        logger.info(retrieved_content)
 
-            # Appending recevied message and llm message to database
-            message += [{"role": "AI", "content": llm_response}]
-            task = asyncio.create_task(chat.append_message(message))
+        if "result" not in retrieved_content.keys():  # only contains time
+            pass
+        else:
+            content = ""
+            cont_no = 1
+            for result in retrieved_content["result"]:
+                score = result["score"]
+                if score >= qdrant_config["infer"]["min_score"]:
+                    content += (
+                        f" \n{cont_no}. {result['payload']['text']['stringValue']}"
+                    )
+                    cont_no += 1
 
-            # sending response to user
+            if len(content) >= 1:
+                messages[-1]["content"] = (
+                    "Related Content: " + content + "\n" + messages[-1]["content"]
+                )
+
+        logger.info(messages)
+
+        # llm generation
+        payload = {
+            "messages": messages,
+            "stream": payload.get("stream", True),
+            "max_tokens": llm_config.get("max_tokens", 1024),
+            "temperature": llm_config.get("temperature", 0.7),
+        }
+
+        if payload.get("stream", True):
+            llm_response = self._consume_and_append(
+                llm_response=llm_api.query(payload=payload),
+                chat=chat,
+                messages=messages,
+            )
+            return StreamingHttpResponse(llm_response)
+
+        else:
+            llm_response = await llm_api.query_no_stream(payload=payload)
+            message = {"role": "assistant", "content": content}
+            messages.append(message)
+            logger.info(messages)
+            asyncio.create_task(chat.update_messages(messages))
             return Response(data={"messages": llm_response})
 
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    async def _consume_and_append(self, llm_response, chat, messages):
+        content = ""
+        async for data in llm_response:
+            content += data
+            yield data
+
+        message = {"role": "assistant", "content": content}
+        messages.append(message)
+        logger.info(messages)
+        asyncio.create_task(chat.update_messages(messages))
 
 
 class ChatDeleteAPIView(mixins.DestroyModelMixin, generics.GenericAPIView):
@@ -88,7 +159,8 @@ class ChatDeleteAPIView(mixins.DestroyModelMixin, generics.GenericAPIView):
     serializer_class = ChatSerializer
     lookup_field = "chat_id"
 
-    def delete(self, request, *args, **kwargs):
+    @async_to_sync
+    async def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
 
@@ -97,10 +169,17 @@ class ChatListAPIView(mixins.ListModelMixin, generics.GenericAPIView):
 
     def get_queryset(self):
         user_id = self.kwargs.get("user_id")
-        if user_id:
-            return Chat.objects.filter(user_id=user_id)
-        else:
-            return Chat.objects.all()
+        queryset = Chat.objects.filter(user_id=user_id)
+        return queryset
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    @async_to_sync
+    async def get(self, request, *args, **kwargs):
+        response = self.list(request, *args, **kwargs)
+        return_data = [
+            {"chat_id": data["chat_id"], "title": data["title"]}
+            for data in response.data
+        ]
+        return Response(
+            return_data,
+            status=status.HTTP_200_OK,
+        )
--- server/test/async_client.py
+++ server/test/async_client.py
@@ -1,6 +1,7 @@
 import asyncio
-
+import sys
 import aiohttp
+import json
 
 
 async def fetch_create_data(url, data):
@@ -8,7 +9,7 @@ async def fetch_create_data(url, data):
         async with session.post(url, json=data) as response:
             if response.status == 201:
                 async for word in response.content:
-                    print(word.decode("utf-8").strip())
+                    return json.loads(word.decode("utf-8").strip())
             else:
                 raise Exception(f"Request failed with status code: {response.status}")
 
@@ -18,7 +19,7 @@ async def fetch_get_data(url):
         async with session.get(url) as response:
             if response.status == 200:
                 async for word in response.content:
-                    print(word.decode("utf-8").strip())
+                    return word.decode("utf-8").strip()
             else:
                 raise Exception(f"Request failed with status code: {response.status}")
 
@@ -28,7 +29,7 @@ async def fetch_list_data(url):
         async with session.get(url) as response:
             if response.status == 200:
                 async for word in response.content:
-                    print(word.decode("utf-8").strip())
+                    return json.loads()(word.decode("utf-8").strip())
             else:
                 raise Exception(f"Request failed with status code: {response.status}")
 
@@ -43,34 +44,99 @@ async def fetch_delete_data(url):
                 raise Exception(f"Request failed with status code: {response.status}")
 
 
-async def fetch_update_data(url, data):
+async def fetch_completion_data(url, data):
     async with aiohttp.ClientSession() as session:
-        async with session.patch(url, json=data) as response:
+        async with session.post(url, json=data) as response:
             if response.status == 200:
-                async for word in response.content:
-                    print(word.decode("utf-8").strip())
+                if data["stream"]:
+                    async for line in response.content.iter_any():
+                        yield line.decode("utf-8").strip()
+                else:
+                    text = await response.text()
+                    yield text
             else:
                 raise Exception(f"Request failed with status code: {response.status}")
 
 
 async def main():
-    # url = "http://localhost:8000/v1/chat/" # create
-    # url = "http://localhost:8000/v1/chat/user/550e8400-e29b-41d4-a716-446655440000" # list
-    url = "http://localhost:8000/v1/chat/aa924cf7-7fe3-419f-b382-69ed3ada39bb"  # get (use valid, else will get 404 page not found)
-    # url = "http://localhost:8000/v1/chat/86f7e57c-ae58-4474-9c2a-b83383d6b4d3/delete" # delete (use valid, else will get 404 page not found)
-    # url = "http://localhost:8000/v1/chat/aa924cf7-7fe3-419f-b382-69ed3ada39bb/update" # update (use valid, else will get 404 page not found)
+    import time
 
+    user_id = "550e8400-e29b-41d4-a716-446655440000"
+
+    # create req looks like
+    st = time.time()
+    url = "http://localhost:8000/v1/chat/create"  # create
     data = {
-        "user_id": "550e8400-e29b-41d4-a716-446655440000",  # Example UUID
-        "messages": [{"role": "user", "content": "good"}],  # Example message data
-        # "title": "Apple color dono"  # Example title
+        "user_id": user_id,  # Example UUID
+    }
+    res = await fetch_create_data(url, data)
+    chat_id = res["chat_id"]
+    print(chat_id)
+    print(time.time() - st)
+
+    # chat completion
+    st = time.time()
+    url = f"http://localhost:8000/v1/chat/{chat_id}"  # post for chat completion
+    data = data = {
+        "messages": [
+            {"role": "user", "content": "How are you?"},
+        ],
+        "stream": True,
+        "course": "general",
     }
 
-    # await fetch_create_data(url, data)
-    # await fetch_list_data(url)
+    if data["stream"]:
+        async for lines in fetch_completion_data(url, data):
+            sys.stdout.write(lines + " ")
+            sys.stdout.flush()
+        print("")
+    else:
+        async for res in fetch_completion_data(url, data):
+            print(res + "\n")
+
+    print(time.time() - st)
+
+    st = time.time()
+    url = f"http://localhost:8000/v1/chat/{chat_id}"  # post for chat completion
+    data = data = {
+        "messages": [
+            {"role": "user", "content": "How are you?"},
+            {"role": "assistant", "content": "Anything you wish"},
+            {"role": "user", "content": "This is X"},
+        ],
+        "stream": False,
+        "course": "general",
+    }
+
+    if data["stream"]:
+        async for lines in fetch_completion_data(url, data):
+            sys.stdout.write(lines + " ")
+            sys.stdout.flush()
+        print("")
+    else:
+        async for res in fetch_completion_data(url, data):
+            print(res)
+
+    print(time.time() - st)
+
+    # get list (user_id -> chat_id) mapping
+    st = time.time()
+    url = f"http://localhost:8000/v1/chat/user/{user_id}"  # list
+    res = await fetch_list_data(url)
+    print(res)
+    print(time.time() - st)
+
+    # get_particular chat
+    st = time.time()
+    url = f"http://localhost:8000/v1/chat/{chat_id}/retrieve"  # get (use valid, else will get 404 page not found)
     await fetch_get_data(url)
-    # await fetch_delete_data(url)
-    # await fetch_update_data(url, data)
+    print(time.time() - st)
+
+    # delete the chat
+    st = time.time()
+    url = f"http://localhost:8000/v1/chat/{chat_id}/delete"  # delete (use valid, else will get 404 page not found)
+    await fetch_delete_data(url)
+    print(time.time() - st)
 
 
 if __name__ == "__main__":
